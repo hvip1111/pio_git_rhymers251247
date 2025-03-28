31ec0b5d3551a56e704e7b3bf70e646ddcd0ff16
251168 <251168@edu.p.lodz.pl>
2025-03-28 19:54:04 +0100

    Poprawki zastosowane do podpunktu 6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,9 +3,9 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     public static final int EMPTY_INDEX = -1;
-    public static final int FULL_INDEX = 11;
     public static final int DEFAULT_CAPACITY = 12;
-    public static final int ERROR_CODE = -1;
+    public static final int FULL_INDEX = DEFAULT_CAPACITY - 1;
+    public static final int IF_EMPTY = -1;
 
     private final int[] numbers = new int[DEFAULT_CAPACITY];
 
@@ -26,13 +26,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return ERROR_CODE;
+            return IF_EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return ERROR_CODE;
+            return IF_EMPTY;
         return numbers[total--];
     }
 
